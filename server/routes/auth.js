5d6591bddd5486ed7ae9666f03984ebfab97ffb5
Alex <kavanokami@gmail.com>
2020-07-10 14:49:16 +0400

    feat: add bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
+// Users DB imitation
 const usersDB = [];
 
 /**
@@ -8,7 +10,7 @@ const usersDB = [];
  * @desc   register user
  * @access Public
  */
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { name, password } = req.body;
 
   for (let user of usersDB) {
@@ -17,8 +19,17 @@ router.post('/signup', (req, res) => {
     }
   }
 
-  usersDB.push({ name, password });
-  res.status(201).json({ msg: 'User created' });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = { name, password: hashedPassword };
+    usersDB.push(newUser);
+
+    res.status(201).json({ msg: 'User created' });
+    console.log(usersDB);
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
 });
 
 module.exports = router;
