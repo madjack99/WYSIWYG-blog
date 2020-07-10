27df9a70be6b24b1d94708952d46d3b506fe3ef5
Alex <kavanokami@gmail.com>
2020-07-10 17:39:04 +0400

    feat: add signup logic

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
 
-import { FormWrapper, Form, Button } from './signupStyle';
+import { FormWrapper, Form, Button, Error } from './signupStyle';
 
 const Signup = () => {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  const changeHandler = (e, handler) => {
+    setError(null);
+    const { value } = e.target;
+    handler(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('http://localhost:5000/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, password }),
+    });
+
+    if (res.status === 201) {
+      // add user to redux and authentication: true;
+      // push to blogs page
+      console.log('success');
+    } else {
+      const data = await res.json();
+      const { msg } = data;
+      setError(msg);
+    }
+  };
+
   return (
     <FormWrapper>
-      <Form>
-        <input type='text' placeholder='Name' />
-        <input type='password' placeholder='Password' />
+      {error && <Error>{error}</Error>}
+      <Form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Name'
+          value={name}
+          onChange={(e) => changeHandler(e, setName)}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => changeHandler(e, setPassword)}
+        />
         <Button type='submit'>Sign up</Button>
       </Form>
     </FormWrapper>
--- client/src/components/auth/signupStyle.js
+++ client/src/components/auth/signupStyle.js
@@ -11,10 +11,11 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 100px;
+  min-height: 100px;
   padding: 10px 15px;
   border-radius: 5px;
   background: skyblue;
+  box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.75);
 `;
 
 export const Button = styled.button`
@@ -23,7 +24,19 @@ export const Button = styled.button`
   border-radius: 5px;
   color: #fff;
   cursor: pointer;
+
   &:hover {
     background: darkred;
   }
+`;
+
+export const Error = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  background: red;
+  color: #fff;
+  text-align: center;
 `;
