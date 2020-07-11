5a50a6f7d9058df99db27b7692787df25924d97b
Alex <kavanokami@gmail.com>
2020-07-11 19:26:29 +0400

    feat: add handlers to AddPost

--- client/src/components/posts/AddPost.jsx
+++ client/src/components/posts/AddPost.jsx
@@ -4,21 +4,39 @@ import CKEditor from 'ckeditor4-react';
 import { FormWrapper, Form, Button } from '../../shared/sharedStyles';
 
 const AddPost = () => {
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+
   React.useEffect(() => {
     document.title = 'Добавить запись';
   });
 
   const onEditorChange = (e) => {
     const data = e.editor.getData();
-    console.log(data);
+    setContent(data);
+  };
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, content);
   };
 
   return (
     <FormWrapper>
-      <Form>
-        <input type='text' name='title' placeholder='Post title' />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='title'
+          placeholder='Post title'
+          value={title}
+          onChange={handleChange}
+        />
         <br />
-        <CKEditor onChange={onEditorChange} data='Post content' />
+        <CKEditor onChange={onEditorChange} data={content} />
         <br />
         <Button type='submit'>Add post</Button>
       </Form>
