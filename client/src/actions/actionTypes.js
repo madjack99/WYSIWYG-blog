985060c1d61533cd0dd6631efd7ebc73615d5650
Alex <kavanokami@gmail.com>
2020-07-11 17:23:45 +0400

    feat: add posts reducer

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,1 +1,2 @@
 export const SIGN_UP = 'SIGN_UP';
+export const FETCH_POSTS = 'FETCH_POSTS';
--- client/src/reducer/rootReducer.js
+++ client/src/reducer/rootReducer.js
@@ -1,5 +1,7 @@
 import authReducer from './authReducer';
+import postsReducer from './postsReducer';
 
 export default (state = {}, action) => ({
   isAuthenticated: authReducer(state.isAuthenticated, action),
+  posts: postsReducer(state.posts, action),
 });
