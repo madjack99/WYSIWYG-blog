01f861d0bdb4dc7c554acc608909e402778d8efe
Alex <kavanokami@gmail.com>
2020-07-12 17:39:10 +0400

    feat: change Add post / All Posts link title and path

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { useLocation } from 'react-router-dom';
 
 import { CustomHeader } from './headerStyle';
 import { Button, linkStyle } from '../../shared/sharedStyles';
 
 const Header = () => {
+  const location = useLocation();
+  console.log(location);
+
+  const linkPath = location.pathname === '/add-post' ? 'posts' : 'add-post';
+  const linkTitle =
+    location.pathname === '/add-post' ? 'All posts' : 'Add post';
+
   return (
     <CustomHeader>
       <Button>
-        <Link to='/add-post' css={linkStyle}>
-          Add Post
+        <Link to={linkPath} css={linkStyle}>
+          {linkTitle}
         </Link>
       </Button>
 
