0d4a9fb9fce0ba64a881d018ed6b48ecb0ec587c
Alex <kavanokami@gmail.com>
2020-07-12 18:47:48 +0400

    feat: add styled to Post

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -3,20 +3,26 @@ import parse from 'html-react-parser';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import { DivWrapper } from './postStyle';
+
 const Post = () => {
   const { posts } = useSelector((store) => store);
   const { id } = useParams();
 
   const post = posts.find((post) => post.id === id);
-  console.log(post);
   const { title, author, date, content } = post;
+
+  React.useEffect(() => {
+    document.title = title;
+  });
+
   return (
-    <>
+    <DivWrapper>
       <h1>{title}</h1>
       {parse(content)}
       <p>Author: {author}</p>
       <p>{date}</p>
-    </>
+    </DivWrapper>
   );
 };
 
